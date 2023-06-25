7e097624b947869b92f354e42cbab1b477f70bfd
com.evilcorp <shit09875@gmail.com>
2023-06-25 20:21:05 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from PyQt5 import QtWidgets
+
+from desing import Ui_MainWindow
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
